f4028eb8e3fb2897a79818e1b0c95eaed5cde5c8
wu67 <wk7113@gmail.com>
2023-06-13 15:46:14 +0800

    build: split chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+// import type { PluginOption } from 'vite'
+// import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     base: mode === 'production' ? '/yys-tool-react/' : '/',
-    plugins: [react()],
+    plugins: [
+      react(),
+      // visualizer({
+      //   emitFile: true,
+      //   filename: 'stats.html',
+      // }) as PluginOption,
+    ],
     server: {
       host: '0.0.0.0',
       port: 8888,
@@ -24,7 +32,17 @@ export default defineConfig(({ mode }) => {
       // 构建后是否生成 source map 文件
       sourcemap: mode !== 'production',
       // 启用/禁用 gzip 压缩大小报告
-      reportCompressedSize: mode === 'production',
+      reportCompressedSize: mode !== 'development',
+      // 单文件输出超出该大小会警告, 单位kb
+      chunkSizeWarningLimit: 1000,
+      // roolup 配置项. https://rollupjs.org/configuration-options
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            antd: ['antd'],
+          },
+        },
+      },
     },
   }
 })
